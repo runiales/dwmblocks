e1faae4eba877b0e48d4549df8b28c24d5f7e58e
runiales <irpic.am@gmail.com>
2022-09-03 20:14:42 +0200

    thinkpad

--- config.h
+++ config.h
@@ -1,25 +1,5 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-<<<<<<< HEAD
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/*{"",	"sb-tasks",	10,	26},*/
-	/*{"",	"sb-music",	0,	11},*/
-	/*{"",	"sb-pacpackages",	0,	8},*/
-	{"",	"sb-memory",	10,	14},
-	{"",	"sb-cpubars",	1,	18},
-	/* {"",	"sb-cpu",	10,	18}, */
-	{"",	"sb-moonphase",	18000,	17},
-	{"",	"sb-forecast",	18000,	5},
-	/*{"",	"sb-mailbox",	180,	12},*/
-	/*{"",	"sb-news",	0,	6},*/
-	/*{"",	"sb-torrent",	20,	7},*/
-	/*{"",	"sb-nettraf",	1,	16},*/
-	{"",	"sb-volume",	0,	10},
-	/*{"",	"sb-battery",	5,	3},*/
-	{"",	"sb-clock",	60,	1},
-	{"",	"sb-internet",	5,	4},
-=======
 	/*Icon*/	/*Command*/				/*Update Interval*/	/*Update Signal*/
 	{"",		"tail -1 /home/runiales/.local/share/pomodoro",		0,			28},
 	{"",		"cat /tmp/recordingicon 2>/dev/null",	0,			9},
@@ -40,7 +20,6 @@ static const Block blocks[] = {
 	{"",		"sb-battery",				5,			3},
 	{"",		"sb-clock",				60,			1},
 	{"",		"sb-internet",				5,			4},
->>>>>>> c9c10c9 (preinstall)
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
